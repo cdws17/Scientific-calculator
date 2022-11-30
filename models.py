28d85f1f29b4499d867e805f592ca9762cd6c85a
cdws17 <118676705+cdws17@users.noreply.github.com>
2022-11-30 10:49:02 +0000

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ from flask_login import UserMixin
 from __init__ import db
 
 
+# Initialises the database 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
